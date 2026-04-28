6e872024ae6b1a8e58a5bbcdbc4d3ff320920d99
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-04-28 17:54:50 -0300

    Implementação da Etiqueta Required na classe DTO.

--- Shared/DTOs/AgendamentoDTO.cs
+++ Shared/DTOs/AgendamentoDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,16 @@ namespace Shared.DTOs
         // O ID é mantido para operações de GET, PUT e DELETE
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "O nome do recurso é obrigatório.")]
         public string RecursoNome { get; set; }
         public string RecursoTipo { get; set; }
 
         public DateTime DataInicio { get; set; }
+
+        [Required(ErrorMessage = "A data de término é obrigatória.")]
         public DateTime DataFim { get; set; }
 
+        [Required(ErrorMessage = "O nome do responsável é obrigatório.")]
         public string Responsavel { get; set; }
         public string Departamento { get; set; }
 
