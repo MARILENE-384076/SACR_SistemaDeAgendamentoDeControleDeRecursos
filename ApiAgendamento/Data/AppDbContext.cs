967610ec845003c8103cd66458ec7feeba154d1c
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-04-28 09:29:16 -0300

    Implementação do AppDbContext

---
+++ ApiAgendamento/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiAgendamento.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Shared.Agendamento> Agendamentos { get; set; }
+    }
+}
