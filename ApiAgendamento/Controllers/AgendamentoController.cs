7ffc33d47222f42529ded6fcdea8974cd66f5938
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-04-28 13:04:02 -0300

    Criando o Controller como API Controller - Empty e criando o construtor do banco.

---
+++ ApiAgendamento/Controllers/AgendamentoController.cs
@@ -0,0 +1,21 @@
+﻿using ApiAgendamento.Data; 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; 
+using Shared; 
+
+namespace ApiAgendamento.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgendamentoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        // O CONSTRUTOR:O ASP.NET injeta o banco para usar na variável _context.
+        public AgendamentoController(AppDbContext context)
+        {
+            _context = context;
+        }
+                
+    }
+}
