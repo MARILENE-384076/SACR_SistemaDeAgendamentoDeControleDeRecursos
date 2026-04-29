f80c026d906b7016937fb4d8ac86fd4782b93780
Marilene da Silva Araujo <maryllenearaujo@gmail.com>
2026-04-29 14:36:20 -0300

    Implementação do código AgendamentoSimulador.

--- AgendamentoSimulador/Program.cs
+++ AgendamentoSimulador/Program.cs
@@ -2,7 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
-using Shared; // Certifique-se que o Shared está referenciado
+using Shared; 
 
 namespace AgendamentoSimulador
 {
